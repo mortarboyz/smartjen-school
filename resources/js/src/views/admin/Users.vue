13a2dce9a2edf69d3522a907eee52a7b4f091c59
Rahmat Yani Hidayat <mortarboyz@gmail.com>
2021-10-22 13:37:00 +0700

    Add User Form

--- resources/js/src/views/admin/Users.vue
+++ resources/js/src/views/admin/Users.vue
@@ -1,29 +1,60 @@
 <template>
-  <v-tabs v-model="tab" align-with-title>
-    <v-tab v-for="item in items" :key="item">
-      {{ item }}
-    </v-tab>
-    <v-tabs-items v-model="tab">
-      <v-tab-item>
+  <div>
+    <v-toolbar flat>
+      <v-spacer></v-spacer>
+      <!-- Add User Dialog -->
+      <v-dialog max-width="500px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+            Add User
+          </v-btn>
+        </template>
+        <AddDialog />
+      </v-dialog>
+      <!-- Invite User Dialog -->
+      <v-dialog max-width="500px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="primary"
+            dark
+            class="mb-2 ml-2"
+            v-bind="attrs"
+            v-on="on"
+          >
+            Invite User
+          </v-btn>
+        </template>
+      </v-dialog>
+    </v-toolbar>
+    <v-tabs v-model="tab" grow>
+      <v-tab v-for="item in items" :key="item">
+        {{ item }}
+      </v-tab>
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
           <TableList />
-      </v-tab-item>
-      <v-tab-item>
+        </v-tab-item>
+        <v-tab-item>
           <TableList />
-      </v-tab-item>
-    </v-tabs-items>
-  </v-tabs>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-tabs>
+  </div>
 </template>
 <script>
-import TableList from '../../_components/TableList.vue';
+import TableList from "../../_components/TableList.vue";
+import AddDialog from "./_components/AddDialog.vue";
 export default {
   data() {
     return {
       tab: null,
       items: ["Teacher", "Student"],
+      dialogs: false,
     };
   },
   components: {
-      TableList,
-  }
+    TableList,
+    AddDialog
+  },
 };
 </script>
---
+++ resources/js/src/views/admin/_components/AddDialog.vue
@@ -0,0 +1,140 @@
+<template>
+  <form>
+    <v-card>
+      <v-card-title class="text-h5 grey lighten-2"> Add User </v-card-title>
+
+      <v-card-text>
+        <v-text-field
+          v-model="form.email"
+          :error-messages="emailErrors"
+          label="Email"
+          required
+          @input="$v.form.email.$touch()"
+          @blur="$v.form.email.$touch()"
+        ></v-text-field>
+        <v-text-field
+          v-model="form.username"
+          :error-messages="usernameErrors"
+          label="Username"
+          required
+          @input="$v.form.username.$touch()"
+          @blur="$v.form.username.$touch()"
+        ></v-text-field>
+        <v-text-field
+          v-model="form.password"
+          :error-messages="passwordErrors"
+          label="Password"
+          type="password"
+          required
+          @input="$v.form.password.$touch()"
+          @blur="$v.form.password.$touch()"
+        ></v-text-field>
+        <v-select
+          v-model="form.role"
+          :items="data.roles"
+          :error-messages="roleErrors"
+          label="Role"
+          required
+          @change="$v.form.role.$touch()"
+          @blur="$v.form.role.$touch()"
+        ></v-select>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" class="mr-4" @click="submit"> submit </v-btn>
+        <v-btn @click="dialog = false"> cancel </v-btn>
+      </v-card-actions>
+    </v-card>
+  </form>
+</template>
+
+<script>
+import { validationMixin } from "vuelidate";
+import { required, email, minLength, integer } from "vuelidate/lib/validators";
+
+export default {
+  props: {
+    invite: Boolean,
+  },
+  data() {
+    return {
+      form: {
+        email: null,
+        username: null,
+        password: null,
+        role: null,
+      },
+      data: {
+        roles: [
+          {
+            text: "Teacher",
+            value: 1,
+          },
+          {
+            text: "Student",
+            value: 2,
+          },
+        ],
+      },
+    };
+  },
+
+  mixins: [validationMixin],
+
+  validations: {
+    form: {
+      email: { required, email },
+      username: { required, minLength: minLength(8) },
+      password: { required, minLength: minLength(8) },
+      role: { required, integer },
+    },
+  },
+
+  computed: {
+    emailErrors() {
+      const errors = [];
+      if (!this.$v.form.email.$dirty) return errors;
+      !this.$v.form.email.email && errors.push("Must be valid e-mail.");
+      !this.$v.form.email.required && errors.push("E-mail is required.");
+      return errors;
+    },
+    usernameErrors() {
+      const errors = [];
+      if (!this.$v.form.username.$dirty) return errors;
+      !this.$v.form.username.minLength &&
+        errors.push("Username must be at most 8 characters long.");
+      !this.$v.form.username.required && errors.push("Username is required.");
+      return errors;
+    },
+    passwordErrors() {
+      const errors = [];
+      if (!this.$v.form.password.$dirty) return errors;
+      !this.$v.form.password.minLength &&
+        errors.push("Password must be at most 8 characters long.");
+      !this.$v.form.password.required && errors.push("Password is required.");
+      return errors;
+    },
+    roleErrors() {
+      const errors = [];
+      if (!this.$v.form.role.$dirty) return errors;
+      !this.$v.form.role.integer && errors.push("Must be a number.");
+      !this.$v.form.role.required && errors.push("Role is required.");
+      return errors;
+    },
+  },
+
+  methods: {
+    submit() {
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
+        // Add API
+        console.log(this.form);
+        this.$v.reset();
+      }
+    },
+  },
+};
+</script>
