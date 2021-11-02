d91931a10c6d946c48b504ee3f548b689820021a
Rahmat Yani Hidayat <mortarboyz@gmail.com>
2021-11-02 10:05:37 +0700

    Chat View

--- resources/js/src/teacher/router.js
+++ resources/js/src/teacher/router.js
@@ -1,17 +1,19 @@
-import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const Login = () => import('./views/Login.vue');
 const Base = () => import('./views/Base.vue');
 const Users = () => import('./views/Users.vue');
+const Chat = () => import('./views/Chat.vue');
 
 let routes = [
     { path: '/login', component: Login },
     {
         path: '', component: Base, children: [
             { path: '', redirect: 'users' },
-            { path: 'users', component: Users }
+            { path: 'users', component: Users },
         ]
     },
+    { path: '/chat/:id', component: Chat }
 ];
 
 export default createRouter({
